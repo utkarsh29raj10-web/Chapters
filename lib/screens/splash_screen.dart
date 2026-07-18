0234a2491237d714783feb69dd508e220d303064
Utkarsh Raj <utkarsh29raj10@gmail.com>
2026-07-18 13:38:36 +0530

    added animations to splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import '../database/database_helper.dart';
 import 'themed_logo.dart';
 
@@ -13,19 +12,27 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  late Animation<double> _scaleAnimation;
-  late Animation<double> _fadeAnimation;
+  late Animation<double> _bgOpacityAnimation;
+  late Animation<double> _logoScaleAnimation;
+  late Animation<double> _shineAnimation;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 1200));
+    _controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 2500));
 
-    _scaleAnimation = Tween<double>(begin: 0.8, end: 1.0)
-      .animate(CurvedAnimation(parent: _controller, curve: Curves.elasticOut));
+    _bgOpacityAnimation = Tween<double>(begin: 0.9, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: const Interval(0.0, 0.4, curve: Curves.easeOut)),
+    );
 
-    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0)
-      .animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+    _logoScaleAnimation = TweenSequence<double>([
+      TweenSequenceItem(tween: Tween(begin: 0.8, end: 1.05).chain(CurveTween(curve: Curves.easeOutCubic)), weight: 40),
+      TweenSequenceItem(tween: Tween(begin: 1.05, end: 1.0).chain(CurveTween(curve:Curves.easeInOut)), weight: 60),
+    ]).animate(CurvedAnimation(parent: _controller, curve: const Interval(0.1, 0.8)));
+
+    _shineAnimation = Tween<double>(begin: -1.0, end: 2.0).animate(
+      CurvedAnimation(parent: _controller, curve: const Interval(0.4, 0.9, curve: Curves.easeInOut)),
+    );
 
     _controller.forward();
     _initializeApp();
@@ -45,26 +52,45 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
     _controller.dispose();
     super.dispose();
   }
-
   @override
   Widget build(BuildContext context) {
-    final isDark = Theme.of(context).brightness == Brightness.dark;
+    final theme = Theme.of(context);
 
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      body: FadeTransition(
-        opacity: _fadeAnimation,
-        child: Center(
-          child: ScaleTransition(
-            scale: _scaleAnimation,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const ThemedLogo(size: 120),
-              ],
+      backgroundColor: theme.colorScheme.surface,
+      body: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return Opacity(
+            opacity: _bgOpacityAnimation.value,
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: theme.colorScheme.surface,
+              child: Center(
+                child: Transform.scale(
+                  scale: _logoScaleAnimation.value,
+                  child: ShaderMask(
+                    blendMode: BlendMode.srcATop,
+                    shaderCallback: (bounds) {
+                      return LinearGradient(
+                        begin: Alignment(0, _shineAnimation.value - 1),
+                        end: Alignment(0, _shineAnimation.value + 1),
+                        colors: [
+                          Colors.white.withOpacity(0.0),
+                          Colors.white.withOpacity(0.4),
+                          Colors.white.withOpacity(0.0),
+                        ],
+                        stops: const [0.0, 0.5, 1.0],
+                      ).createShader(bounds);
+                    },
+                    child: const ThemedLogo(size: 120),
+                  ),
+                ),
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
